96cfc88ceefc5c9d08caa5055186bd85c837dd19
gabrieudev <gabrieudev@gmail.com>
2026-07-26 14:23:52 -0300

    first commit

---
+++ src/main/java/com/audit/tracevault/TracevaultApplication.java
@@ -0,0 +1,13 @@
+package com.audit.tracevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracevaultApplication.class, args);
+	}
+
+}
